1a8c43f38254ccbbeedfd24938a187e2e60e6199
awl8181 <awbeemoo@gmail.com>
2025-10-07 18:11:21 +0700

    Tugas Lab AI
    Update Tugas

---
+++ tugas week 1/verify_installation.py
@@ -0,0 +1,11 @@
+import numpy
+import pandas
+import matplotlib
+import sklearn
+import skfuzzy
+import cv2
+import nltk
+import torch
+import tensorflow
+
+print("All packages imported successfully! OK")
